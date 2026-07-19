65ecf00cbc0ffdd94c38821ef1d55f6517b1bea7
Igor Cherednyk <igor@Ukraine-Igor-Cherednyk.local>
2026-07-19 23:25:13 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/english-learner-app/sw.js', { scope: '/english-learner-app/' })})}
